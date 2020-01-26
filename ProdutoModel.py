3b5d38cd8c7aa7efe104d8551eec111be595af1f
Mario Fernandes <marioantonio340@gmail.com>
2020-01-26 17:34:13 -0300

    Crud

---
+++ ProdutoModel.py
@@ -0,0 +1,44 @@
+import pymysql
+
+conexao = pymysql.connect(
+    host='localhost',
+    user='root',
+    passwd='',
+    database='teste'
+)
+
+
+def insereProduto(nome, preco, quantidade):
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO produto(nome, preco, quantidade) VALUES('{}',{},{})".format(nome, preco, quantidade))
+    conexao.commit()
+
+
+def deletaProduto(id):
+    cursor = conexao.cursor()
+    cursor.execute("DELETE FROM produto WHERE idproduto = {}".format(id))
+    conexao.commit()
+
+
+def alteranome(nome, id):
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE produto SET nome = {} WHERE idproduto = {}".format(nome, id))
+    conexao.commit()
+
+
+def alteraPreco(preco, id):
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE produto SET preco = {} WHERE idproduto = {}".format(preco, id))
+    conexao.commit()
+
+
+def alteraQtd(qtd, id):
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE produto SET quantidade = {} WHERE idproduto = {}".format(qtd, id))
+    conexao.commit()
+
+
+def listarTodos():
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM produto")
+    return conexao.commit()
